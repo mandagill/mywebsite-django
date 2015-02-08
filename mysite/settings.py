57a606b4a06e172aadacfa7facada9a09509c7ef
Amanda G <ms.amgilmore@gmail.com>
2015-02-07 16:11:29 -0800

    fixed a typo in the logic to load local versus server settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -84,7 +84,7 @@ USE_TZ = False
 STATIC_URL = '/static/'
 
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
@@ -93,7 +93,6 @@ ALLOWED_HOSTS = ['*']
 STATIC_ROOT = 'staticfiles'
 
 DEBUG = False
-At the end of the mysite/settings.py, copy and paste this:
 
 try:
     from .local_settings import *
